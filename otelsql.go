71b02b6403669a91230257e109d9a693e7c39dea
Sean Slattery <sean.slattery203@gmail.com>
2020-08-29 14:26:58 -0700

    hard code function names

--- otelsql.go
+++ otelsql.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"database/sql/driver"
-	"runtime"
 
 	"github.com/ngrok/sqlmw"
 	"go.opentelemetry.io/otel/api/global"
@@ -38,7 +37,7 @@ type sqlInterceptor struct {
 }
 
 func (in *sqlInterceptor) ConnBeginTx(ctx context.Context, conn driver.ConnBeginTx, txOpts driver.TxOptions) (driver.Tx, error) {
-	ctx, span := in.tr.Start(ctx, defaultName(ctx), trace.WithSpanKind(trace.SpanKindClient),
+	ctx, span := in.tr.Start(ctx, "ConnBeginTx", trace.WithSpanKind(trace.SpanKindClient),
 		trace.WithAttributes(in.traceAttributes...))
 	defer span.End()
 	return conn.BeginTx(ctx, txOpts)
@@ -46,14 +45,14 @@ func (in *sqlInterceptor) ConnBeginTx(ctx context.Context, conn driver.ConnBegin
 
 func (in *sqlInterceptor) ConnPrepareContext(ctx context.Context, conn driver.ConnPrepareContext, query string) (driver.Stmt, error) {
 	traceAttributes := append(in.traceAttributes, kv.String("sql.query", query))
-	ctx, span := in.tr.Start(ctx, defaultName(ctx), trace.WithSpanKind(trace.SpanKindClient),
+	ctx, span := in.tr.Start(ctx, "ConnPrepareContext", trace.WithSpanKind(trace.SpanKindClient),
 		trace.WithAttributes(traceAttributes...))
 	defer span.End()
 	return conn.PrepareContext(ctx, query)
 }
 
 func (in *sqlInterceptor) ConnPing(ctx context.Context, conn driver.Pinger) error {
-	ctx, span := in.tr.Start(ctx, defaultName(ctx), trace.WithSpanKind(trace.SpanKindClient),
+	ctx, span := in.tr.Start(ctx, "ConnPing", trace.WithSpanKind(trace.SpanKindClient),
 		trace.WithAttributes(in.traceAttributes...))
 	defer span.End()
 	return conn.Ping(ctx)
@@ -61,7 +60,7 @@ func (in *sqlInterceptor) ConnPing(ctx context.Context, conn driver.Pinger) erro
 
 func (in *sqlInterceptor) ConnExecContext(ctx context.Context, conn driver.ExecerContext, query string, args []driver.NamedValue) (driver.Result, error) {
 	traceAttributes := append(in.traceAttributes, kv.String("sql.query", query))
-	ctx, span := in.tr.Start(ctx, defaultName(ctx), trace.WithSpanKind(trace.SpanKindClient),
+	ctx, span := in.tr.Start(ctx, "ConnExecContext", trace.WithSpanKind(trace.SpanKindClient),
 		trace.WithAttributes(traceAttributes...))
 	defer span.End()
 	return conn.ExecContext(ctx, query, args)
@@ -69,14 +68,14 @@ func (in *sqlInterceptor) ConnExecContext(ctx context.Context, conn driver.Exece
 
 func (in *sqlInterceptor) ConnQueryContext(ctx context.Context, conn driver.QueryerContext, query string, args []driver.NamedValue) (driver.Rows, error) {
 	traceAttributes := append(in.traceAttributes, kv.String("sql.query", query))
-	ctx, span := in.tr.Start(ctx, defaultName(ctx), trace.WithSpanKind(trace.SpanKindClient),
+	ctx, span := in.tr.Start(ctx, "ConnQueryContext", trace.WithSpanKind(trace.SpanKindClient),
 		trace.WithAttributes(traceAttributes...))
 	defer span.End()
 	return conn.QueryContext(ctx, query, args)
 }
 
 func (in *sqlInterceptor) ConnectorConnect(ctx context.Context, connect driver.Connector) (driver.Conn, error) {
-	ctx, span := in.tr.Start(ctx, defaultName(ctx), trace.WithSpanKind(trace.SpanKindClient),
+	ctx, span := in.tr.Start(ctx, "ConnectorConnect", trace.WithSpanKind(trace.SpanKindClient),
 		trace.WithAttributes(in.traceAttributes...))
 	defer span.End()
 	return connect.Connect(ctx)
@@ -84,7 +83,7 @@ func (in *sqlInterceptor) ConnectorConnect(ctx context.Context, connect driver.C
 
 func (in *sqlInterceptor) ResultLastInsertId(res driver.Result) (int64, error) {
 	ctx := context.Background()
-	_, span := in.tr.Start(ctx, defaultName(ctx), trace.WithSpanKind(trace.SpanKindClient),
+	_, span := in.tr.Start(ctx, "ResultLastInsertId", trace.WithSpanKind(trace.SpanKindClient),
 		trace.WithAttributes(in.traceAttributes...))
 	defer span.End()
 	return res.LastInsertId()
@@ -92,62 +91,50 @@ func (in *sqlInterceptor) ResultLastInsertId(res driver.Result) (int64, error) {
 
 func (in *sqlInterceptor) ResultRowsAffected(res driver.Result) (int64, error) {
 	ctx := context.Background()
-	_, span := in.tr.Start(ctx, defaultName(ctx), trace.WithSpanKind(trace.SpanKindClient),
+	_, span := in.tr.Start(ctx, "ResultRowsAffected", trace.WithSpanKind(trace.SpanKindClient),
 		trace.WithAttributes(in.traceAttributes...))
 	defer span.End()
 	return res.RowsAffected()
 }
 
 func (in *sqlInterceptor) RowsNext(ctx context.Context, rows driver.Rows, dest []driver.Value) error {
-	_, span := in.tr.Start(ctx, defaultName(ctx), trace.WithSpanKind(trace.SpanKindClient),
+	_, span := in.tr.Start(ctx, "RowsNext", trace.WithSpanKind(trace.SpanKindClient),
 		trace.WithAttributes(in.traceAttributes...))
 	defer span.End()
 	return rows.Next(dest)
 }
 
 func (in *sqlInterceptor) StmtExecContext(ctx context.Context, stmt driver.StmtExecContext, _ string, args []driver.NamedValue) (driver.Result, error) {
-	ctx, span := in.tr.Start(ctx, defaultName(ctx), trace.WithSpanKind(trace.SpanKindClient),
+	ctx, span := in.tr.Start(ctx, "StmtExecContext", trace.WithSpanKind(trace.SpanKindClient),
 		trace.WithAttributes(in.traceAttributes...))
 	defer span.End()
 	return stmt.ExecContext(ctx, args)
 }
 
 func (in *sqlInterceptor) StmtQueryContext(ctx context.Context, stmt driver.StmtQueryContext, _ string, args []driver.NamedValue) (driver.Rows, error) {
-	ctx, span := in.tr.Start(ctx, defaultName(ctx), trace.WithSpanKind(trace.SpanKindClient),
+	ctx, span := in.tr.Start(ctx, "StmtQueryContext", trace.WithSpanKind(trace.SpanKindClient),
 		trace.WithAttributes(in.traceAttributes...))
 	defer span.End()
 	return stmt.QueryContext(ctx, args)
 }
 
 func (in *sqlInterceptor) StmtClose(ctx context.Context, stmt driver.Stmt) error {
-	_, span := in.tr.Start(ctx, defaultName(ctx), trace.WithSpanKind(trace.SpanKindClient),
+	_, span := in.tr.Start(ctx, "StmtClose", trace.WithSpanKind(trace.SpanKindClient),
 		trace.WithAttributes(in.traceAttributes...))
 	defer span.End()
 	return stmt.Close()
 }
 
 func (in *sqlInterceptor) TxCommit(ctx context.Context, tx driver.Tx) error {
-	_, span := in.tr.Start(ctx, defaultName(ctx), trace.WithSpanKind(trace.SpanKindClient),
+	_, span := in.tr.Start(ctx, "TxCommit", trace.WithSpanKind(trace.SpanKindClient),
 		trace.WithAttributes(in.traceAttributes...))
 	defer span.End()
 	return tx.Commit()
 }
 
 func (in *sqlInterceptor) TxRollback(ctx context.Context, tx driver.Tx) error {
-	_, span := in.tr.Start(ctx, defaultName(ctx), trace.WithSpanKind(trace.SpanKindClient),
+	_, span := in.tr.Start(ctx, "TxRollback", trace.WithSpanKind(trace.SpanKindClient),
 		trace.WithAttributes(in.traceAttributes...))
 	defer span.End()
 	return tx.Rollback()
-}
-
-func defaultName(ctx context.Context) string {
-	pc, _, _, ok := runtime.Caller(3)
-	if !ok {
-		return "OtelSQLInvalidRuntimeName"
-	}
-	f := runtime.FuncForPC(pc)
-	if f == nil {
-		return "OtelSQLInvalidRuntimeName"
-	}
-	return f.Name()
 }
